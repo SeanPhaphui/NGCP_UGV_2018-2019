04e1d14dfe1716a44ed7b8da1397b4c2c1525e51
DylanWReyes <dylanwilliamreyes@gmail.com>
2019-05-03 12:33:21 -0700

    Added functions for UDPs

--- UGVComputerVision/UGV Full CV Code.py
+++ UGVComputerVision/UGV Full CV Code.py
@@ -17,6 +17,50 @@ bottleSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bcenterSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 
+def ballsend(x, y, ballfound, phase):
+    ballbyte = bytearray(struct.pack("i", int(x)))
+    ballbyte += bytearray(struct.pack("i", int(y)))
+    ballbyte += bytearray(struct.pack("i", int(ballfound)))
+    bcenterSock.sendto(ballbyte, (UDP_IP_ADDRESS, udp2))
+          
+    RecieverSock.setblocking(0)
+    try:
+        data, address = RecieverSock.recvfrom(1024)
+        phase = int.from_bytes(data,byteorder='little')
+    except socket.error:
+        phase = 1
+
+    return phase
+        
+    
+    
+
+
+def bottlesend(cX, cY, anglefound, bottlefound, phase):
+    bottlebyte = bytearray(struct.pack("i", int(anglefound)))
+    bottlebyte += (bytearray(struct.pack("i", int(cX))))
+    bottlebyte += (bytearray(struct.pack("i", int(cY))))
+    bottlebyte += (bytearray(struct.pack("i", int(bottlefound))))
+    bottleSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    bottleSock.sendto(bottlebyte, (UDP_IP_ADDRESS, udp1))
+    print('Angle found: ', anglefound)
+    print('Center X coordinate: ', cX)
+    print('Center Y coordinate: ', cY)
+    
+    RecieverSock.setblocking(0)
+    try:
+        data, address = RecieverSock.recvfrom(1024)
+        phase = int.from_bytes(data,byteorder='little')
+    except socket.error:
+        phase = 2
+
+
+    return phase
+
+
+    
+
+
 def update(x):
     pass
 
@@ -24,7 +68,6 @@ def update(x):
 
 
 RecieverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-##RecieverSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 RecieverSock.bind((UDP_IP_ADDRESS, udp_reciever))
 data, address = RecieverSock.recvfrom(1024)
 phase = int.from_bytes(data,byteorder='little')
@@ -39,23 +82,26 @@ cv2.createTrackbar("MaxLum", "UGV Filter",0,2550,update)
 cv2.createTrackbar("MinLum", "UGV Filter",0,2550,update)
 
 cv2.setTrackbarPos("MaxHue", "UGV Filter",1800)
-cv2.setTrackbarPos("MinHue", "UGV Filter",1590)
-cv2.setTrackbarPos("MaxSat", "UGV Filter",1000)
-cv2.setTrackbarPos("MinSat", "UGV Filter",270)
-cv2.setTrackbarPos("MaxLum", "UGV Filter",2370)
-cv2.setTrackbarPos("MinLum", "UGV Filter",0)
+cv2.setTrackbarPos("MinHue", "UGV Filter",1671)
+cv2.setTrackbarPos("MaxSat", "UGV Filter",2063)
+cv2.setTrackbarPos("MinSat", "UGV Filter",556)
+cv2.setTrackbarPos("MaxLum", "UGV Filter",2154)
+cv2.setTrackbarPos("MinLum", "UGV Filter",571)
 
-x =0
-y= 0
-r= 0
+x = 0
+y = 0
+ballfound = 0
+ta = 500
+
+counter = 0
 while phase == 1:
 
     
     _, image = maincam.read()
 
     
-
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HLS)
+    output = image.copy()
+    hsl = cv2.cvtColor(image, cv2.COLOR_BGR2HLS)
     maxhue =(cv2.getTrackbarPos("MaxHue", "UGV Filter"))/10
     minhue = (cv2.getTrackbarPos("MinHue", "UGV Filter"))/10
     maxsat=(cv2.getTrackbarPos("MaxSat", "UGV Filter"))/10
@@ -65,54 +111,120 @@ while phase == 1:
     
     lowh = np.array([minhue, minlum, minsat])
     upph = np.array([maxhue, maxlum, maxsat])
+    lowh2 = np.array([51.6, 11.4, 128.6])
+    upph2 = np.array([180.0, 57.9, 255.0])
     
-    mask = cv2.inRange(hsv, lowh, upph)
-    edges = cv2.Canny(mask,150,200)
-    ret,thresh = cv2.threshold(mask, 40, 255, 0) 
-    radius = 24.324324324324326
-    ksize = int(6 * round(radius) + 1)
-    output = image.copy()
-    res2=cv2.GaussianBlur(thresh,(ksize, ksize), round(radius))
-    circles = cv2.HoughCircles(res2, cv2.HOUGH_GRADIENT, 1, 200, param1=30, param2=65, minRadius=50, maxRadius=0)
-    ballfound = 0 
-    if circles is not None:
-		# convert the (x, y) coordinates and radius of the circles to integers
-
-        ballfound = 1;
-        circles = np.round(circles[0, :]).astype("int")
-		
-		# loop over the (x, y) coordinates and radius of the circles
-        for (x, y, r) in circles:
-            cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-            cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-                #time.sleep(0.5)
-            print ("X coordinate:")
-            print (x)
-            print ("Y coordinate: ")
-            print (y)
-            print ("Radius is: ")
-            print (r)
-
+    
+    mask = cv2.inRange(hsl, lowh, upph)
+    mask2 = cv2.inRange(hsl, lowh2, upph2)
+    total = cv2.add(mask, mask2)
+    
+    
+##    edges = cv2.Canny(mask,150,200)
+    ret,thresh = cv2.threshold(total, 40, 255, 0)
+    im2,contours,hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    
+      
+    if counter == 0:
+        ballfound = 0
         
-            ballbyte = bytearray(struct.pack("i", int(x)))
-            ballbyte += bytearray(struct.pack("i", int(y)))
-            ballbyte += bytearray(struct.pack("i", int(ballfound)))
-            bcenterSock.sendto(ballbyte, (UDP_IP_ADDRESS, udp2))
+        
 
+    if len(contours) != 0:
+        c = max(contours, key = cv2.contourArea)
+        M = cv2.moments(c)
+        a = cv2.contourArea(c)
+        if a > ta:
+            if counter == 5:                     
+                ballfound =  1
 
-    ballbyte = bytearray(struct.pack("i", int(x)))
-    ballbyte += bytearray(struct.pack("i", int(y)))
-    ballbyte += bytearray(struct.pack("i", int(ballfound)))
-    bcenterSock.sendto(ballbyte, (UDP_IP_ADDRESS, udp2))
-    cv2.imshow('mask',res2)
-    cv2.imshow('image',output)
+            cv2.drawContours(output, c, -1, (0, 0, 255), 3)
+            x = int(M["m10"] / M["m00"])
+            y = int(M["m01"] / M["m00"])
+                                                
+            cv2.circle(output, (x, y), 7, (255, 255, 255), -1)
+
+            cv2.putText(output, "center", (x - 20, y - 20),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            if ta < 1500:
+                ta = ta + 10
+            if counter < 5:
+                counter = counter + 1
+        
     
-    RecieverSock.setblocking(0)
-    try:
-        data, address = RecieverSock.recvfrom(1024)
-        phase = int.from_bytes(data,byteorder='little')
-    except socket.error:
-         phase = 1
+            cv2.imshow('mask', total)
+            cv2.imshow('image',output)
+
+            phase = ballsend(x, y, ballfound, phase)
+
+                   
+##            ballbyte = bytearray(struct.pack("i", int(x)))
+##            ballbyte += bytearray(struct.pack("i", int(y)))
+##            ballbyte += bytearray(struct.pack("i", int(ballfound)))
+##            bcenterSock.sendto(ballbyte, (UDP_IP_ADDRESS, udp2))
+##            
+##            
+##            RecieverSock.setblocking(0)
+##            try:
+##                data, address = RecieverSock.recvfrom(1024)
+##                phase = int.from_bytes(data,byteorder='little')
+##            except socket.error:
+##                phase = 1
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                maincam.release()
+                cv2.destroyAllWindows()
+                break
+
+            continue
+        
+        else:
+            counter = 0
+            ta = 500
+            ballfound = 0
+
+            cv2.imshow('mask', total)
+            cv2.imshow('image',output)
+
+            phase = ballsend(x, y, ballfound, phase)
+            
+##            ballbyte = bytearray(struct.pack("i", int(x)))
+##            ballbyte += bytearray(struct.pack("i", int(y)))
+##            ballbyte += bytearray(struct.pack("i", int(ballfound)))
+##            bcenterSock.sendto(ballbyte, (UDP_IP_ADDRESS, udp2))
+##            
+##            
+##            RecieverSock.setblocking(0)
+##            try:
+##                data, address = RecieverSock.recvfrom(1024)
+##                phase = int.from_bytes(data,byteorder='little')
+##            except socket.error:
+##                phase = 1
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                maincam.release()
+                cv2.destroyAllWindows()
+                break
+
+            continue
+        
+    cv2.imshow('mask', total)
+    cv2.imshow('image',output)
+
+    phase = ballsend(x, y, ballfound, phase)
+    
+##    ballbyte = bytearray(struct.pack("i", int(x)))
+##    ballbyte += bytearray(struct.pack("i", int(y)))
+##    ballbyte += bytearray(struct.pack("i", int(ballfound)))
+##    bcenterSock.sendto(ballbyte, (UDP_IP_ADDRESS, udp2))
+##
+##
+##    RecieverSock.setblocking(0)
+##    try:
+##        data, address = RecieverSock.recvfrom(1024)
+##        phase = int.from_bytes(data,byteorder='little')
+##    except socket.error:
+##         phase = 1
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         maincam.release()
@@ -148,111 +260,152 @@ cY = 0
 
 while phase==2:
 
-        _, image = maincam.read()
-        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HLS)
-        maxhue =(cv2.getTrackbarPos("MaxHue", "UGV Filter"))/10
-        minhue = (cv2.getTrackbarPos("MinHue", "UGV Filter"))/10
-        maxsat=(cv2.getTrackbarPos("MaxSat", "UGV Filter"))/10
-        minsat=(cv2.getTrackbarPos("MinSat", "UGV Filter"))/10
-        maxlum=(cv2.getTrackbarPos("MaxLum", "UGV Filter"))/10
-        minlum=(cv2.getTrackbarPos("MinLum", "UGV Filter"))/10
+    _, image = maincam.read()
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HLS)
+    maxhue =(cv2.getTrackbarPos("MaxHue", "UGV Filter"))/10
+    minhue = (cv2.getTrackbarPos("MinHue", "UGV Filter"))/10
+    maxsat=(cv2.getTrackbarPos("MaxSat", "UGV Filter"))/10
+    minsat=(cv2.getTrackbarPos("MinSat", "UGV Filter"))/10
+    maxlum=(cv2.getTrackbarPos("MaxLum", "UGV Filter"))/10
+    minlum=(cv2.getTrackbarPos("MinLum", "UGV Filter"))/10
+
+    lowh = np.array([minhue, minlum, minsat])
+    upph = np.array([maxhue, maxlum, maxsat])
     
-        lowh = np.array([minhue, minlum, minsat])
-        upph = np.array([maxhue, maxlum, maxsat])
-        
 
-        output = image.copy()
-        mask = cv2.inRange(hsv, lowh, upph)
+    output = image.copy()
+    mask = cv2.inRange(hsv, lowh, upph)
 
-        
-       
-        ret,thresh = cv2.threshold(mask, 40, 255, 0)
-        
-        im2,contours,hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    
+   
+    ret,thresh = cv2.threshold(mask, 40, 255, 0)
+    
+    im2,contours,hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-        if ta < 2000:
-            ta = ta + 10
+    if ta < 1500:
+        ta = ta + 10
 
-       
-        if counter == 0:
-            bottlefound = 0
-        
-        
+   
+    if counter == 0:
+        bottlefound = 0
+    
+    
 
-        if len(contours) != 0:
-                c = max(contours, key = cv2.contourArea)
-                M = cv2.moments(c)
-                a = cv2.contourArea(c)
-                if a > ta:
-                        if counter == 5:                     
-                            bottlefound =  1
-                          
-                        cv2.drawContours(image, c, -1, 255, 3)
-                        cX = int(M["m10"] / M["m00"])
-                        cY = int(M["m01"] / M["m00"])
-                                                
-                        cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
+    if len(contours) != 0:
+            c = max(contours, key = cv2.contourArea)
+            M = cv2.moments(c)
+            a = cv2.contourArea(c)
+            if a > ta:
+                    if counter == 5:                     
+                        bottlefound =  1
+                      
+                    cv2.drawContours(image, c, -1, 255, 3)
+                    cX = int(M["m10"] / M["m00"])
+                    cY = int(M["m01"] / M["m00"])
+                                            
+                    cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
 
-                        cv2.putText(image, "center", (cX - 20, cY - 20),
-                                       cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                        
-                        rows,cols = image.shape[:2]
-                        [vx,vy,x,y] = cv2.fitLine(c, cv2.DIST_L2,0,0.01,0.01)
-                        left = int((-x*vy/vx) + y)
-                        right = int(((cols-x)*vy/vx)+y)
-                        image = cv2.line(image,(cols-1,right),(0,left),(0,255,0),2)
+                    cv2.putText(image, "center", (cX - 20, cY - 20),
+                                   cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                    
+                    rows,cols = image.shape[:2]
+                    [vx,vy,x,y] = cv2.fitLine(c, cv2.DIST_L2,0,0.01,0.01)
+                    left = int((-x*vy/vx) + y)
+                    right = int(((cols-x)*vy/vx)+y)
+                    image = cv2.line(image,(cols-1,right),(0,left),(0,255,0),2)
 
-                        if(right > cY):
-                                anglefound = math.atan2(right-cY, (cols-1) - cX ) * (180/3.141592653589793)
-                        else:
-                                anglefound = 180 + math.atan2(right-cY, (cols-1) - cX ) * (180/3.141592653589793)
+                    if(right > cY):
+                            anglefound = math.atan2(right-cY, (cols-1) - cX ) * (180/3.141592653589793)
+                    else:
+                            anglefound = 180 + math.atan2(right-cY, (cols-1) - cX ) * (180/3.141592653589793)
 
-                        bottlebyte = bytearray(struct.pack("i", int(anglefound)))
-                        bottlebyte += (bytearray(struct.pack("i", int(cX))))
-                        bottlebyte += (bytearray(struct.pack("i", int(cY))))
-                        bottlebyte += (bytearray(struct.pack("i", int(bottlefound))))
-                        bottleSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                        bottleSock.sendto(bottlebyte, (UDP_IP_ADDRESS, udp1))
-                        
-                        if counter < 5:
-                            counter = counter + 1
+                    if counter < 5:
+                        counter = counter + 1
 
-                        print('Angle found: ', anglefound)
-                        print('Center X coordinate: ', cX)
-                        print('Center Y coordinate: ', cY)
-                else:
-                        counter = 0
-                        bottlefound = 0
+                    
+                    cv2.imshow("image", image)
+                    cv2.imshow("mask", mask)
 
-        else:
-            counter = 0
-            bottlefound = 0
-                        
-                        
-                        
-        
-        
-        bottlebyte = bytearray(struct.pack("i", int(anglefound)))
-        bottlebyte += (bytearray(struct.pack("i", int(cX))))
-        bottlebyte += (bytearray(struct.pack("i", int(cY))))
-        bottlebyte += (bytearray(struct.pack("i", int(bottlefound))))
-        bottleSock.sendto(bottlebyte, (UDP_IP_ADDRESS, udp1))
-        
+                    phase = bottlesend(cX, cY, anglefound, bottlefound,phase)
+                    
+##                    bottlebyte = bytearray(struct.pack("i", int(anglefound)))
+##                    bottlebyte += (bytearray(struct.pack("i", int(cX))))
+##                    bottlebyte += (bytearray(struct.pack("i", int(cY))))
+##                    bottlebyte += (bytearray(struct.pack("i", int(bottlefound))))
+##                    bottleSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+##                    bottleSock.sendto(bottlebyte, (UDP_IP_ADDRESS, udp1))
+##                    
+##                    print('Angle found: ', anglefound)
+##                    print('Center X coordinate: ', cX)
+##                    print('Center Y coordinate: ', cY)
+##                    
+##                    RecieverSock.setblocking(0)
+##                    try:
+##                        data, address = RecieverSock.recvfrom(1024)
+##                        phase = int.from_bytes(data,byteorder='little')
+##                    except socket.error:
+##                        phase = 2
+                    
+
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        maincam.release()
+                        cv2.destroyAllWindows()
+                        break
+                    
+                    continue
+                    
+            else:
+                cv2.imshow("image", image)
+                cv2.imshow("mask", mask)
+                counter = 0
+                bottlefound = 0
+                phase = bottlesend(cX, cY, anglefound, bottlefound,phase)
+##                bottlebyte = bytearray(struct.pack("i", int(anglefound)))
+##                bottlebyte += (bytearray(struct.pack("i", int(cX))))
+##                bottlebyte += (bytearray(struct.pack("i", int(cY))))
+##                bottlebyte += (bytearray(struct.pack("i", int(bottlefound))))
+##                bottleSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+##                bottleSock.sendto(bottlebyte, (UDP_IP_ADDRESS, udp1))   
+##                print('Angle found: ', anglefound)
+##                print('Center X coordinate: ', cX)
+##                print('Center Y coordinate: ', cY)
+##
+##                RecieverSock.setblocking(0)
+##                try:
+##                    data, address = RecieverSock.recvfrom(1024)
+##                    phase = int.from_bytes(data,byteorder='little')
+##                except socket.error:
+##                    phase = 2
+
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    maincam.release()
+                    cv2.destroyAllWindows()
+                    break
+                
+                continue
             
-        cv2.imshow("image", image)
-        cv2.imshow("mask", mask)
+    cv2.imshow("image", image)
+    cv2.imshow("mask", mask)
+    phase = bottlesend(cX, cY, anglefound, bottlefound,phase)
+##    bottlebyte = bytearray(struct.pack("i", int(anglefound)))
+##    bottlebyte += (bytearray(struct.pack("i", int(cX))))
+##    bottlebyte += (bytearray(struct.pack("i", int(cY))))
+##    bottlebyte += (bytearray(struct.pack("i", int(bottlefound))))
+##    bottleSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+##    bottleSock.sendto(bottlebyte, (UDP_IP_ADDRESS, udp1))   
+##    
+##
+##    RecieverSock.setblocking(0)
+##    try:
+##        data, address = RecieverSock.recvfrom(1024)
+##        phase = int.from_bytes(data,byteorder='little')
+##    except socket.error:
+##        phase = 2
 
-        RecieverSock.setblocking(0)
-        try:
-            data, address = RecieverSock.recvfrom(1024)
-            phase = int.from_bytes(data,byteorder='little')
-        except socket.error:
-            phase = 2
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            maincam.release()
-            cv2.destroyAllWindows()
-            break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        maincam.release()
+        cv2.destroyAllWindows()
+        break
 
 
 maincam.release()
